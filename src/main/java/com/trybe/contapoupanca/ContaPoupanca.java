8dccf36a581ba4eecd4a83fb8a460b029cb428cb
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-08-09 14:48:43 -0300

    Initial commit.

--- src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,28 @@
 package com.trybe.contapoupanca;
 
+/** Classe ContaPoupanca. */
 public class ContaPoupanca {
+  private double saldo;
+  private String titularConta;
+  
+  public ContaPoupanca(double saldo, String titularConta) {
+    this.saldo = saldo;
+    this.titularConta = titularConta;
+  }
 
+  public void depositar(Double valor) {
+    this.saldo = this.saldo + valor;
+  }
+
+  public void sacar(Double valor) {
+    this.saldo = this.saldo - valor;
+  }
+  
+  public double mostrarSaldo() {
+    return this.saldo;
+  }
+
+  public String mostrarTitularConta() {
+    return this.titularConta;
+  }
 }
